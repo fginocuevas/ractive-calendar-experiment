fa7159c53dae0acb7ea0f4987968a7e42cb34a9b
eman027 <emmanuel.portillo@pointwest.com.ph>
2017-07-19 17:06:16 +0800

    Added add event function in calendar.

--- js/myCalendar.js
+++ js/myCalendar.js
@@ -1,5 +1,34 @@
+var selectedDate = "";
+var prevSelectedElement;
+
 $(function(){
+    $("#addEventModalContainer1").hide();
+
+
+    var events = JSON.parse(sessionStorage.getItem('events'));
+
+    if (events =="" || events == null){
+        events = mockEvents();
+        sessionStorage.setItem('events', JSON.stringify(events));
+    }
+    generateCal(events);
+
+    $("#sampleId").on('click',function(){
+
+        var userEvent = $("#eventTitle").val();
+        events.push({ date: selectedDate, title: userEvent})
+        sessionStorage.setItem('events', JSON.stringify(events));
+        //var sessionEvents = sessionStorage.getItem("events");
+        window.location="../ractive-calendar-experiment/myCalendar.html";
+    });
+
+
+
+});
+
+function generateCal(myEvents){
     $('#generateCalendarContainer').clndr({
+
         render: function(data){
 
             showAdjacentMonths: false,
@@ -25,7 +54,7 @@ $(function(){
             data.weeks= weeks;
 
             // Print out contents of data into the console
-            console.log(JSON.stringify(data)); //output the json object in console for inspection.
+            //console.log(JSON.stringify(data)); //output the json object in console for inspection.
 
             var calendarRactiveOptions= {
                 el: '#generateCalendarContainer',
@@ -37,8 +66,15 @@ $(function(){
 
         },
 
+        events : myEvents,
+
         clickEvents: {
                     click: function (target) {
+                        $("#addEventModalContainer1").show();
+                        $(".calendar-day-"+prevSelectedElement).removeClass("light-blue-background");
+                        selectedDate = target.date._i;
+                        prevSelectedElement = selectedDate;
+                        $(".calendar-day-"+selectedDate).addClass("light-blue-background");
                         console.log('Cal-1 clicked: ', target);
                     },
                     today: function () {
@@ -77,4 +113,29 @@ $(function(){
         //daysOfTheWeek	: ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'],
 
     })
-})
+};
+
+function showAddEventModal(){
+    var calendarRactiveOptions= {
+        el: '#addEventModalContainer',
+        template: '#addEventModalTmpl'
+    }
+
+    var calendarRactive= new Ractive(calendarRactiveOptions);
+}
+
+function addEvent(events){
+
+}
+
+
+function mockEvents(){
+	var events = new Array();
+	var event2 = { date: '2017-08-03', title: 'BIRTHDAY!'};
+	var event3 = { date: '2017-07-12', title: 'Independence Day!'};
+	events.push(event2);
+	events.push(event3);
+
+	return events;
+};
+
