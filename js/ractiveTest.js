675b8730cefd81292da8a7ab8880a3b0a340b38f
eman027 <emmanuel.portillo@pointwest.com.ph>
2017-07-19 10:42:37 +0800

    Added planet function.

--- js/ractiveTest.js
+++ js/ractiveTest.js
@@ -10,6 +10,14 @@ var view = new Ractive({
     data: model
 });
 
+$('#addPlanets').on('click',function(){
+    var myPlanets = new Ractive({
+        el: '#solarSystemHolder',
+        template: '#solarSystemTemplate',
+        data: planets
+    });
+});
+
 $('#activate').on('click', function () {
     view.set({"name":"Francis", "employeeNum": 177777});
 });
